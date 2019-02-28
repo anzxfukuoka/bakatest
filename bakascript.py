daeaf4d7ed08cccb752c4e23b9a2f07d05a36753
Anzx <0.love.2d.atsi.2@gmail.com>
2019-02-28 21:42:00 +0200

    ✩

--- bakascript.py
+++ bakascript.py
@@ -1,4 +1,5 @@
 import telebot
+import random as rand
 
 token = "556161272:AAFoP3abycmprZXz3NpjfwRuZ6PVwelkLpw"
 
@@ -8,6 +9,17 @@ bot = telebot.TeleBot(token)
 def send_welcome(message):
     print(message)
     bot.reply_to(message, "start!")
+
+@bot.message_handler(commands=['roll'])
+def send_welcome(message):
+    roll = rand.randint(0,100)
+    bot.send_message(message.chat.id, roll)
+
+@bot.message_handler(commands=['me'])
+def send_welcome(message):
+    bot.send_message(message.chat.id,
+    "_" + message.from_user.first_name + " " + message.text.replace("/me","") + "_",
+    parse_mode='Markdown')
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
